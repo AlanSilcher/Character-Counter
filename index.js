8b3eaf8c11878979950c61d9b67661641f84301d
AlanSilcher <silcheralan@gmail.com>
2023-05-08 10:52:19 -0300

    Terminacion sin estilos

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function getId (id) {
-    /* console.log("esto es value", document.getElementById(id).value) */
     return document.getElementById(id).value
 }
 
 function innerHTML (id, result) {
-   /*  console.log("esto es html", document.getElementById(id).innerHTML=result) */
     return document.getElementById(id).innerHTML=result
 }
 
@@ -12,7 +10,6 @@ function characterCounter () {
 
     setInterval(function (){
         var count = getId("text")
-        console.log("esto es count", count)
         innerHTML("textView", count.length)
     },1000)
 }

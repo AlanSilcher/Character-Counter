44ae665324e3b3342c0bea8934bfc2018fafbec2
AlanSilcher <silcheralan@gmail.com>
2023-05-08 10:48:28 -0300

    Console.log

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function getId (id) {
-    console.log(document.getElementById(id).value)
+    /* console.log("esto es value", document.getElementById(id).value) */
     return document.getElementById(id).value
 }
 
 function innerHTML (id, result) {
+   /*  console.log("esto es html", document.getElementById(id).innerHTML=result) */
     return document.getElementById(id).innerHTML=result
 }
 
@@ -11,7 +12,7 @@ function characterCounter () {
 
     setInterval(function (){
         var count = getId("text")
-        console.log(count)
+        console.log("esto es count", count)
         innerHTML("textView", count.length)
-    },0000)
+    },1000)
 }
